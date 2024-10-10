14d90ccfd4894a1da645162614c36b2ca98d90be
Felipe Marques <felipemr94@outlook.com>
2024-10-10 08:46:54 -0300

    Project init

---
+++ SwiftyDS/SwiftyDS/_App/SwiftyDSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyDSApp.swift
+//  SwiftyDS
+//
+//  Created by Felipe Marques Ramos on 09/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyDSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
