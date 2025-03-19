d8e093b90c7a6a1a7494acd20dc12ff4169597fa
Felipe Marques <felipemr94@outlook.com>
2025-03-19 18:06:11 -0300

    Basic main view

--- SwiftyDS/SwiftyDS/_App/ContentView.swift
+++ SwiftyDS/SwiftyDS/_App/ContentView.swift
@@ -8,15 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+	
+	let algoList = ["Big O Notation", "Search", "Sorting Algorithms", "Dynamic Programming", "Recursion", "Backtracking", "Greedy","Two Pointers", "Slinding Window", "Bit Manipulation", "Tortoise and the Hare"]
+	let dsList = ["Array", "Linked List", "Stack", "Queue", "Tree", "Graph", "Hash Table", "Trie", "Heap"]
+	
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+		NavigationStack {
+			List {
+				Section("Algorithms") {
+					ForEach(algoList, id: \.self) { algo in
+						contentCell(algo)
+					}
+				}
+				Section("Data Structures") {
+					ForEach(dsList, id: \.self) { ds in
+						contentCell(ds)
+					}
+				}
+			}
+		}
     }
+	
+	@ViewBuilder
+	func contentCell(_ title: String) -> some View {
+		NavigationLink(title) {
+			Text("Info about \(title)")
+		}
+	}
 }
 
 #Preview {
